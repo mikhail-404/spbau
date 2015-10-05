f50bb0441e80ad2a67784033d4781be2249aa860
Mikhail Mitrofanov <mikhail404@gmail.com>
2015-10-06 01:04:50 +0300

    Доравботана обработка аргументов командной строки

--- Huffman/src/main.cpp
+++ Huffman/src/main.cpp
@@ -4,29 +4,34 @@
 
 #include "../include/binary_writer.hpp"
 #include "../include/binary_reader.hpp"
-#include <map>
+#include "../include/command_parser.hpp"
 
 int main(int argc, char *argv[])
 {
-    std::ifstream in(argv[1], std::ios::in);
+    //
+    GetOptions options(argc, argv);
 
-    if (!in.is_open())
+    //
+    std::string input_file, output_file;
+    input_file  = options.input_file();
+    output_file = options.output_file();
+
+    std::ifstream in(input_file, std::ios::binary | std::ios::in);
+    if (in.is_open())
     {
-        std::cout << "File is not found" << std::endl;
-        return -1;
+        std::ofstream out(output_file, std::ios::binary | std::ios::out);
+        //
+        if (options.is_compress())
+        {
+            std::cout << "Encode" << std::endl;
+            encoder(in, out);
+        }
+        else if (options.is_uncompress())
+        {
+            std::cout << "Decode" << std::endl;
+            decoder(in, out);
+        }
     }
-
-    std::ofstream out(argv[2], std::ios::binary | std::ios::out);
-
-    encoder(in, out);
-
-    in.close();
-    out.close();
-
-    std::ifstream in2(argv[2], std::ios::binary | std::ios::in);
-    std::ofstream out2("../input2", std::ios::out | std::ios::binary);
-
-    decoder(in2, out2);
 
     return 0;
 }
