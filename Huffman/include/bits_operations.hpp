425279d1239acbdede56975117b06b406ad54836
Mikhail Mitrofanov <mikhail404@gmail.com>
2015-10-13 21:21:57 +0300

    Added latest version of Huffman code/decode

--- Huffman/include/bits_operations.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef BITS_OPERATIONS_HPP
-#define BITS_OPERATIONS_HPP
-
-int            set_bit        (int number, unsigned char i);
-unsigned char  get_bit        (int number, unsigned char i);
-int            clear_bit      (int number, unsigned char i);
-int            clear_all_bits (int number, unsigned char i);
-
-#endif // BITS_OPERATIONS_HPP
--- Huffman/include/features.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef FEATURES_HPP
-#define FEATURES_HPP
-
-const char          SIGN_END_TABLE = 0;
-const unsigned char BITS_SIZE      = 31;
-
-#endif // FEATURES_HPP
